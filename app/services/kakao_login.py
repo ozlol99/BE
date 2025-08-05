98c367c4f551ff96dc717dfae3a8e341f1ef07f1
park3hho <ejfhr7777@gmail.com>
2025-08-05 18:35:29 +0900

    Linted
    Signed-off-by: park3hho <ejfhr7777@gmail.com>

--- app/services/kakao_login.py
+++ app/services/kakao_login.py
@@ -1,4 +1,7 @@
-import os, dotenv, requests
+import os
+
+import dotenv
+import requests
 from fastapi import HTTPException
 
 dotenv.load_dotenv()
@@ -6,6 +9,7 @@ dotenv.load_dotenv()
 KAKAO_REST_API_KEY = os.getenv("KAKAO_REST_API_KEY")
 REDIRECT_URI = "http://127.0.0.1:8000/kakao-login"
 KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token"
+
 
 # 2. 토큰 요청 함수 (인가 코드를 인자로 받음)
 def request_kakao_token(code: str):
@@ -44,11 +48,10 @@ def request_kakao_token(code: str):
         print(f"토큰 요청 중 오류 발생: {e}")
         raise HTTPException(status_code=500, detail="토큰 발급 실패")
 
+
 def get_kakao_profile(access_token):
     url = "https://kapi.kakao.com/v2/user/me"
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
+    headers = {"Authorization": f"Bearer {access_token}"}
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # HTTP 오류 발생 시 예외 발생
