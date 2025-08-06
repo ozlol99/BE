f31f5a685440c4e96d6949beca8d21e2f1a452ec
wjdtjddns98 <wjdtjddns98@naver.com>
2025-08-06 13:51:57 +0900

    google_auth feature Dev

--- app/models/user.py
+++ app/models/user.py
@@ -12,13 +12,15 @@ class UserModel(models.Model):
     id = fields.IntField(pk=True, auto_create=True)
     email = fields.CharField(unique=True, max_length=255, description="이메일")
     user = fields.CharField(null=True, max_length=255, description="닉네임")
+    gender = fields.IntField(null=True, description="성별 (null 가능)")
+    birthday = fields.DateField(null=True, description="생년월일 (null 가능)")
     riot_user = fields.CharField(
         unique=True, max_length=255, null=True, description="라이엇 계정"
     )
     google_or_kakao = fields.CharEnumField(
         Social, null=False, description="소셜 계정 정보"
     )
-    likes = fields.IntField(description="좋아요")
+    likes = fields.IntField(default=0, description="좋아요")
     created_at = fields.DatetimeField(auto_now=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
