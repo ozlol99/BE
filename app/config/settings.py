c696e0ce3fdf00869ef7013b17401cf1dafbd232
wjdtjddns98 <wjdtjddns98@naver.com>
2025-08-19 20:21:35 +0900

    ♻️ refactor: 웹소켓 연결시 파라미터로 토큰 연결 했던 방식에서, 헤더로 넘겨주는 방식 채택 후 코드 수정
    ✨ feat: 새로운 기능 추가
    🐛 fix: 버그 수정
    🎨 style: 코드 포맷팅, 스타일 변경 (기능 변경 없음)
    ♻️ refactor: 코드 리팩터링
    ✅ test: 테스트 코드 추가 또는 수정
    🔨 chore: 빌드 관련 설정 변경 또는 보조 작업
    🚀 deploy: 배포 관련 작업
    🗑️ remove: 파일 또는 코드 삭제

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,6 @@
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import Annotated
 
 
 class Settings(BaseSettings):
@@ -28,5 +28,6 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     base_url: str
+
 
 settings = Settings()
