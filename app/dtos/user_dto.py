728f5569a4ad603b3dfed8109b64f572d27defab
wjdtjddns98 <wjdtjddns98@naver.com>
2025-08-24 15:36:46 +0900

    ✨ feat: user_router.py GET user/me api 할시 riot_accounts도 List로 반환하도록 수정  -> 프론트단에서 방 생성시에 GET user/me 로 연동된 라이엇 계정을 드롭다운으로 나타내주기 위함
    🗑️ remove: 필요없는 chat_room_dto.py 삭제
    
    ✨ feat: 새로운 기능 추가
    🐛 fix: 버그 수정
    🎨 style: 코드 포맷팅, 스타일 변경 (기능 변경 없음)
    ♻️ refactor: 코드 리팩터링
    ✅ test: 테스트 코드 추가 또는 수정
    🔨 chore: 빌드 관련 설정 변경 또는 보조 작업
    🚀 deploy: 배포 관련 작업
    🗑️ remove: 파일 또는 코드 삭제

--- app/dtos/user_dto.py
+++ app/dtos/user_dto.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
@@ -19,3 +19,20 @@ class UserUpdate(BaseModel):
     user: str = Field(..., max_length=255, description="닉네임")
     gender: Optional[bool] = Field(None, description="성별 (true: 남성, false: 여성)")
     birthday: Optional[datetime] = Field(None, description="생년월일")
+
+
+class RiotAccountResponse(BaseModel):
+    id: int
+    game_name: str
+    tag_line: str
+
+    class Config:
+        from_attributes = True
+
+
+class UserMeResponse(BaseModel):
+    id: int
+    email: str
+    user: str
+    google_or_kakao: str
+    riot_accounts: List[RiotAccountResponse]
